5bf8646f604ce17984afcb379c27529fa80b8a4c
Marcos Guillermo <53832367+MarcosDaNight@users.noreply.github.com>
2020-04-02 12:32:05 -0300

    Update Government Spending.py

--- Government Spending.py
+++ Government Spending.py
@@ -1,16 +1,16 @@
+
 from matplotlib import pyplot as plt
 
 #data speding from Federal Government Plan
-planes = ['Transferência para saúde', 'FPE e FPM', 'Assistência Social', 'Suspensão de dívidas dos estados com a União', 'Renegociação com bancos', 'federais', 'crédito']
+planes = ['Transferência para saúde', 'FPE e FPM', 'Assistência Social', 'Suspensão de dívidas da União', 'Renegociação com bancos']
 
-spend = [8, 16, 2, 12.6, 9.6]
+spend = [8, 16, 2, 12.6, 9.3]
 
-xs = [i + 0.1 for i, _ in enumerate(planes)]
+plt.axis("equal")
 
-plt.bar(xs, spend)
+plt.pie(spend, labels=planes, autopct='%1.2f%%')
 
-plt.ylabel("Spending for Bilions of R$")
-plt.title("Sepending for Federal Governmnt Plan ")
+plt.title("Sepending for Federal Governmnt Plan")
 
-plt.xticks([i + 0.5 for i, _ in enumerate(planes)], planes)
-plt.show()
+
+
